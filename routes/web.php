0992523d4395081fccd057ff3d5ce5028d6204c2
Boris <bpzweber@gmail.com>
2019-05-22 16:09:28 +0200

    Backend Admin Authentication

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,10 @@
 |
 */
 
-Route::get('/', function () {
+require 'admin.php';
+
+/*Route::get('/', function () {
     return view('welcome');
 });
 
-Route::view('/admin', 'admin.dashboard.index');
+Route::view('/admin', 'admin.dashboard.index');*/
